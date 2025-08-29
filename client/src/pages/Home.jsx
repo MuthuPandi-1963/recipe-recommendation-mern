4811e14ae462d4f0ba918e27a836804f26ce5ea1
thenraja01 <thenwthen@gmail.com>
2025-08-29 10:49:51 +0530

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='w-full min-h-screen grid place-content-center my-2 gap-2'>
+      <h1 className='text-4xl font-bold grid '><span>Welcome</span> to RecipeRec</h1>
+      <p className='text-gray-500 text-lg grid'>Explore the Recipe with sweetie ,spicy and worldwide <Button className="w-fit">lognin</Button></p>
+    </div>
   )
 }
