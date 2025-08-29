5edadb84ebe09a2b5c54e6bf852ddb86bb09954d
thenraja01 <thenwthen@gmail.com>
2025-08-29 17:06:27 +0530

    card process static

--- client/src/pages/Cards.jsx
+++ client/src/pages/Cards.jsx
@@ -2,15 +2,27 @@ import {CardHeader,Card,CardAction,CardContent,CardFooter,CardDescription,CardTi
 import Samplecards from './samplecard'
 export default function Cards() {
     return(
-       <div className="flex overflow-x-auto min-w-2xl scroll-auto ">
+        <div>
+                <h1 className='font-bold text-xl py-4'>Recently viewed</h1>
+       <div className="grid grid-cols-1 gap-5 overflow-x-auto  border  min-w-full ">
+        <div className="flex space-x-4 min-w-max">
+
         <Samplecards/>
         <Samplecards/>
         <Samplecards/>
         <Samplecards/>
-        <Samplecards/><Samplecards/><Samplecards/><Samplecards/><Samplecards/><Samplecards/><Samplecards/>
+        <Samplecards/>
+        <Samplecards/>
+        <Samplecards/>
+        <Samplecards/>
+        <Samplecards/>
+        <Samplecards/>
+        <Samplecards/>
         <Samplecards/>
         <Samplecards/>
 
+        </div>
+        </div>
        </div>
     )
 };
