e319f5c4af443a74bef7a16e589ae87d0ff771d1
thenraja01 <thenwthen@gmail.com>
2025-08-29 11:06:35 +0530

    buttons log

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { useNavigate } from 'react-router-dom'
 export default function Home() {
+  const navigate =useNavigate()
+  const HandleOnClick =()=>{
+    navigate('/auth')}
   return (
-    <div className='w-full min-h-screen grid place-content-center my-2 gap-2'>
+    <div className='w-full min-h-screen grid place-content-center  gap-2'>
       <h1 className='text-4xl font-bold grid '><span>Welcome</span> to RecipeRec</h1>
-      <p className='text-gray-500 text-lg grid'>Explore the Recipe with sweetie ,spicy and worldwide <Button className="w-fit">lognin</Button></p>
+      <p className='text-gray-500 text-lg grid md:flex md:gap-6 '>Explore the Recipe with sweetie ,spicy and worldwide <Button className="w-fit my-2" onClick={HandleOnClick}>Log In</Button></p>
     </div>
   )
 }
