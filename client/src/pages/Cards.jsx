feba025c464bbeec28e5f919dcb8b0d45a26eef5
thenraja01 <thenwthen@gmail.com>
2025-08-30 14:37:05 +0530

    card useContext

--- client/src/pages/Cards.jsx
+++ client/src/pages/Cards.jsx
@@ -1,26 +1,12 @@
 import {CardHeader,Card,CardAction,CardContent,CardFooter,CardDescription,CardTitle} from '../components/ui/card'
-import Samplecards from './samplecard'
+import Samplecards from '../utilities/Samplecard'
 export default function Cards() {
     return(
         <div>
                 <h1 className='font-bold text-xl py-4'>Recently viewed</h1>
        <div className="grid grid-cols-1 gap-5 overflow-x-auto  border  min-w-full ">
         <div className="flex space-x-4 min-w-max">
-
         <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-        <Samplecards/>
-
         </div>
         </div>
        </div>
--- client/src/utilities/Samplecard.jsx
+++ client/src/utilities/Samplecard.jsx
@@ -11,29 +11,37 @@ export default function Samplecards() {
     const HandleOnclik=()=>{
             navigate('recipe:id')
     }
+    console.log();
+    
+    
     const liking=()=>{
             Setlike(!like)
     }
     return(
-        <div className="relative">
-
-        <Card >
-            <CardTitle>{value.name}</CardTitle>
-            <CardHeader>chilli soup</CardHeader>
+        <div className=" flex   gap-5 my-4">
+         {value.Provider.map((card)=>{
+            return(
+             <Card className='p-4 m-2 relative ' >
+            <CardTitle className='text-gray-600 font-semibold text-lg'>{card.name}</CardTitle>
+            <CardHeader className='text-start p-0 font-bold min-h-fit text-2xl'>{card.category}</CardHeader>
             <CardAction>
-                {like&& <div className='absolute top-2 right-2 '>
+                {like&&<div className='absolute top-2 right-2 '>
                     <FcLike size={24} onClick={()=>{Setlike(false)}}/>
-                    </div>}
-               
+                    </div>}             
             </CardAction>
-            <CardFooter className='w-full gap-2 '>
+                <CardContent>{card.tags}</CardContent>
+                <CardDescription>{card.ingredients}</CardDescription>
+            <CardFooter className=' gap-2 grid '>
+            <CardContent>{card.rating}</CardContent>
+                <h1>{card.price}</h1>
                 <Button onClick={HandleOnclik}>view desc</Button>
                   <Button className='bg-gray-700 py-0 ' onClick={liking}>
                     favorite
                     </Button>
             </CardFooter>
 
-        </Card>
+        </Card>);
+        })}   
        
         </div>
     )
