66975a53d33c6179755952476a470846c5cfcc46
thenraja01 <thenwthen@gmail.com>
2025-08-31 16:51:53 +0530

    profile

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+export default function Profile() {
+    return(
+        // <div className="p-4 font-bold text-4xl min-h-screen place-content-center ">
+        // <h1>profile</h1>
+        // <p className="text-gray-600 font-serif">no profile found ....</p>
+        // </div>
+        <div>
+            
+            
+        </div>
+    )
+    
+};
