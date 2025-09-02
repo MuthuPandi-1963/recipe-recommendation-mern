b0959a19a76af5abdd8fa1a58391b3fe8f9c6b79
thenraja01 <thenwthen@gmail.com>
2025-09-02 13:23:05 +0530

    utils add

---
+++ client/src/utils/Validator.jsx
@@ -0,0 +1,65 @@
+export  function ValidatorEmailorPhone(value) {
+    if(!value.trim()){
+        return 'email or phone is required'
+    }
+    const emailregex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phoneregex=/^\+?\d{10,15}$/;
+    if(!emailregex.test(value) && !phoneregex.test(value)){
+        return "enter a valid mobile or email"
+    }
+    return null
+};
+
+export function Username(value){
+    if(value.string()||null){
+    if(!value.trim()){
+        return "User name required"
+    }
+    if(value.trim().lenght<2 ){
+        return "Enter Full name must be least 3"
+    }}
+    return null
+}
+export function Password(password){
+if(!password){
+    return "Password is required"}
+ if (password.length < 8) {
+    return "Password must be at least 8 characters";
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    return "Password must contain at least one uppercase letter";
+  }
+
+  if (!/[a-z]/.test(password)) {
+    return "Password must contain at least one lowercase letter";
+  }
+
+  if (!/[0-9]/.test(password)) {
+    return "Password must contain at least one number";
+  }
+
+  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    return "Password must contain at least one special character";
+  }
+  return null
+}
+export function ConformPassword(password,ConformPassword){
+if(!ConformPassword){
+        return "confirm password is required"
+    }
+    if(password!==ConformPassword){
+        return "password must be same check it !"
+    }
+    return null
+
+}
+export function ValidateIsPhone(data) {
+  const phoneRegex = /^\+?\d{10,15}$/;
+  return phoneRegex.test(data);
+}
+
+export function ValidateIsEmail(data) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(data);
+}
